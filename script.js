b2cde9b8103ac598dab7f979e77222bbe4a027f9
Maciej grze <mistermakos.mcg@gmail.com>
2023-07-14 23:20:32 +0200

    you cant reapeat tasks when updating them.

--- script.js
+++ script.js
@@ -23,6 +23,17 @@ if (localStorage.getItem("Tasks") !== null)
     });
 }
 
+const check_value = (value) =>
+{
+    if(Tasks.includes(value) == true){
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 //Submiting form (adding tasks to list)
 const add_task = () => 
 {
@@ -30,8 +41,7 @@ const add_task = () =>
     if(value == ""){alert("You have to name your tasks!");}
     else
     {
-        if(Tasks.includes(value) == true){alert("Dont repeat yourself!");}
-        else{
+        if(check_value(value)){
             Tasks.push(value); 
             print_task(value)
         }
@@ -54,12 +64,16 @@ const done = (c) =>
 const replace = (c) => 
 {
     let new_task = c.value;
-    let nodes = c.parentNode.children;
-    Tasks[Tasks.indexOf(nodes[1].textContent)] = new_task
-    nodes[1].textContent = new_task;
-    nodes[1].style.display = "block";
-    nodes[2].blur(); // it deletes annoying bug that occured sometimes
-    c.remove();
+    if(check_value(new_task))
+    {
+        let nodes = c.parentNode.children;
+        Tasks[Tasks.indexOf(nodes[1].textContent)] = new_task
+        nodes[1].textContent = new_task;
+        nodes[1].style.display = "block";
+        nodes[2].blur(); // it deletes annoying bug that occured sometimes
+        c.remove();
+    }
+    else{ c.blur(); return 0;}
 }
 
 const check_key = (c,key) => 
